d91ee7d095c51e632f07375d81e9e62153a2b45e
Stenver <jerkku.stenver@gmail.com>
2023-11-24 21:00:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 document.getElementById('package').addEventListener('click', function() {
-    var buttons = document.querySelector('.buttons');
-    buttons.classList.toggle('show');
+    var package = document.getElementById('package');
+    var openedPackage = document.getElementById('openedPackage');
+
+    // Change the background image to the opened package
+    package.style.backgroundImage = "url('0217edd2-a956-46ef-b2ef-0cab01214a68.webp')"; // Update this path
+
+    // Show the buttons
+    openedPackage.classList.remove('hidden');
 });
